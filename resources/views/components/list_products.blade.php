9be77e786e01fe499056f67bdf36e3665e66e2be
tonvanhiep <tonvanhiepdragon@gmail.com>
2022-08-16 22:50:07 +0700

    new file:   resources/views/components/list_products.blade.php

---
+++ resources/views/components/list_products.blade.php
@@ -0,0 +1,87 @@
+<div class="list-products-home mb-40">
+    <div class="container">
+        <div class="title-category">{{!isset($title_listproducts)?'Tất cả món ăn':$title_listproducts}}</div>
+        <div class="list-products">
+            <div class="row">
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-1.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Gỏi cuốn</div></a>
+                            <div class="price">7.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-2.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Bánh tráng trộn</div></a>
+                            <div class="price">25.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-3.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Bún đậu mắm tôm</div></a>
+                            <div class="price">70.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-4.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Bánh mì</div></a>
+                            <div class="price">20.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-5.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Bánh bột lọc</div></a>
+                            <div class="price">5.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <div class="item-product">
+                        <a href="/detail-blog.html">
+                            <div class="img-wrapper">
+                                <img src="{{asset('assets/images/products/hinh-6.jpg')}}" alt="Product" />
+                            </div>
+                        </a>
+                        <div class="info-product">
+                            <a href="/detail-blog.html"><div class="name">Sushi/Sashimi</div></a>
+                            <div class="price">7.000 VNĐ</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
